131da3c412caadacad278b1cf151428a2d850e7a
sophiesaunders <81704139+sophiesaunders@users.noreply.github.com>
2024-06-03 13:08:27 -0400

    Initial Commit

---
+++ tunio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tunio
+//
+//  Created by Sophie Saunders on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tunio/tunioApp.swift
@@ -0,0 +1,17 @@
+//
+//  tunioApp.swift
+//  tunio
+//
+//  Created by Sophie Saunders on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct tunioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
