15d7cea3821bb1173b6389290bef5c370e609469
sophiesaunders <81704139+sophiesaunders@users.noreply.github.com>
2024-06-10 20:51:06 -0400

    Fix having empty navigation view, attempted fix for iOS hanging after background, add color

--- tunio/ContentView.swift
+++ tunio/ContentView.swift
@@ -12,48 +12,62 @@ struct ContentView: View {
     // StateObject means that when a Published value changes, we will be notified.
     @StateObject var td = ToneDetector()
     
+    // Track whether app is active, inactive, or in background
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some View {
         
-        NavigationView {
-            VStack {
-                
-                Spacer()
-                
-                HStack {
-                    Text("Frequency")
-                    Text("\(td.data.pitch, specifier: "%0.1f")")
-                }.padding()
-                
-                Spacer()
-                
-                HStack {
-                    Text("Amplitude")
-                    Text("\(td.data.amplitude, specifier: "%0.1f")")
-                }.padding()
-                
-                Spacer()
-                
-                HStack {
-                    Text("Note")
-                    Text(td.data.note)
-                }.padding()
-                
-                Spacer()
-                
-                HStack {
-                    Text("Octave")
-                    Text(String(td.data.octave))
-                }.padding()
-                
-                NodeOutputView(td.tappableB).clipped()
+        VStack {
+            
+            Spacer()
+            
+            HStack {
+                Text(td.data.note)
+                    .font(.system(size: 40, design: .serif))
+            }.padding()
+            
+            HStack {
+                Text("Distance:")
+                Text("\(td.data.distance, specifier: "%0.1f") cents")
             }
-            .navigationTitle("Tunio")
-            .padding()
-            .onAppear() {
-                td.start()
+            
+            HStack {
+                Text("Frequency:")
+                Text("\(td.data.pitch, specifier: "%0.1f") Hz")
             }
-            .onDisappear() {
-                td.stop()
+            
+            HStack {
+                Text("Amplitude:")
+                Text("\(td.data.amplitude, specifier: "%0.1f")")
+            }
+                        
+            HStack {
+                Text("Octave:")
+                Text(String(td.data.octave))
+            }
+            
+            Spacer()
+            
+            NodeOutputView(td.tappableB, color: Color("DarkerGray"), backgroundColor: Color("LighterPink")).clipped()
+            
+            Spacer()
+        }
+        .font(.system(size: 20, design: .serif))
+        .background(Color("LighterPink"))
+        .padding()
+        .onChange(of: scenePhase) { oldPhase, newPhase in
+            if newPhase == .active {
+                if !td.engine.avEngine.isRunning {
+                    td.start()
+                }
+            } else if newPhase == .inactive {
+                if td.engine.avEngine.isRunning {
+                    td.stop()
+                }
+            } else if newPhase == .background {
+                if td.engine.avEngine.isRunning {
+                    td.stop()
+                }
             }
         }
         .task {
